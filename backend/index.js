ffce0ba601743835feea09aa1bf6191f7a540c8e
Akash <akash.youngster789@gmail.com>
2023-09-08 14:36:08 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,27 @@ const Joi = require("joi");
 //     },
 //   });
 
+// const init = async () => {
+//   const server = Hapi.server({
+//     port: process.env.PORT || 3000,
+//     routes: {
+//       cors: {
+//         origin: ['https://sudoku-frontend.vercel.app'],
+//         credentials: true,
+//       },
+//     },
+//   });
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
-    routes: {
-      cors: {
-        origin: ['https://sudoku-frontend.vercel.app'],
-        credentials: true,
-      },
+    port: 3000,
+    host: 'localhost',
+  });
+
+  // Register the hapi-cors plugin with appropriate options
+  await server.register({
+    plugin: HapiCors,
+    options: {
+      origins: ['https://sudoku-frontend.vercel.app'], // Whitelist your frontend domain
     },
   });
 
